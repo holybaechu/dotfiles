08e6ee8511234eafcc98f57c692ba07cb4b3c01e
holybaechu <holyb@holyb.xyz>
2024-04-26 21:30:48 +0900

    [nvim] Change presence neovim_image_text

--- .config/nvim/lua/config/lazy/presence.lua
+++ .config/nvim/lua/config/lazy/presence.lua
@@ -1,4 +1,9 @@
 return {
     'andweeb/presence.nvim',
-    lazy = false
+    lazy = false,
+    config = function()
+        require('presence').setup({
+            neovim_image_text = '"How do I exit this stupid text editor?" - holybaechu',
+        })
+    end
 }
